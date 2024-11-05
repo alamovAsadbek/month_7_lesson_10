94b1b142f691fba1f0c052a24c8c69eb864e2cd9
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:24:49 +0500

    add TweetView class

--- app_tweets/views.py
+++ app_tweets/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import TweetModel
+from .serializers import TweetSerializer
+
+
+class TweetView(generics.GenericAPIView):
+    serializer_class = TweetSerializer
+    queryset = TweetModel.objects.all()
