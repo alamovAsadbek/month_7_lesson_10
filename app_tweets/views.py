efb265b16b7cd5199cd39424579e8f13ee2d4610
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:39:43 +0500

    add TweetPaginationView class

--- app_tweets/views.py
+++ app_tweets/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from .models import TweetModel
 from .serializers import TweetSerializer
+
+
+class TweetPaginationView(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 
 class TweetView(generics.GenericAPIView):
