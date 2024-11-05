c02d617a9a77dffce4a16d4a868a60c6a6476b2b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-05 23:02:24 +0500

    change serializers.oy

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from rest_framework import serializers
 
 
@@ -23,6 +27,15 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password'],
             is_active=False,
         )
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        current_site = get_current_site(self.context['request'])
+        email_subject = 'Activate your account'
+        email_body = f'Hi {user.username},\nPlease use the link below to activate your account:\n{current_site.domain}/verify-email/{uid}/{token}'
+        user.email_user(email_subject, email_body)
+        send_mail(
+            email_subject,
+            
         return user
 
 
