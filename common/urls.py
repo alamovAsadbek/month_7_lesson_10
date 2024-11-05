1df6db64a547020cde84d1921e3174b40fb3092e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:14:14 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('auth/register/', user_views.RegisterView.as_view(), name='register'),
     path('auth/login/', user_views.LoginView.as_view(), name='login'),
     path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
-    path('verify-email/<uidb64>/<token>/', user_views.VerifyEmailView.as_view(), name='verify-email')
+    path('verify-email/<uidb64>/<token>/', user_views.VerifyEmailView.as_view(), name='verify-email'),
+    path('verify-resend/', user_views.ResendEmailVerificationView.as_view(), name='verify-resend')
+
 ]
