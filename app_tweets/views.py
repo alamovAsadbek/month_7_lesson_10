a03dc793803c88da36ad2550ec83ebcadbad23ce
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:25:28 +0500

    change

--- app_tweets/views.py
+++ app_tweets/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import TweetModel
 from .serializers import TweetSerializer
@@ -7,3 +8,8 @@ from .serializers import TweetSerializer
 class TweetView(generics.GenericAPIView):
     serializer_class = TweetSerializer
     queryset = TweetModel.objects.all()
+
+    def get(self, request):
+        tweets = self.get_queryset()
+        serializer = self.get_serializer(tweets, many=True)
+        return Response(serializer.data)
