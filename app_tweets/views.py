8599bc8ee9ca92fe731ca326dc44d79d316dac19
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:40:36 +0500

    change

--- app_tweets/views.py
+++ app_tweets/views.py
@@ -22,3 +22,28 @@ class TweetView(generics.GenericAPIView):
         tweets = self.get_queryset()
         serializer = self.get_serializer(tweets, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        tweet = TweetModel.objects.get(id=pk)
+        tweet.delete()
+        return Response({'message': 'Tweet deleted successfully!'})
+
+    def put(self, request, pk):
+        tweet = TweetModel.objects.get(id=pk)
+        serializer = self.get_serializer(instance=tweet, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        tweet = TweetModel.objects.get(id=pk)
+        serializer = self.get_serializer(instance=tweet, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
