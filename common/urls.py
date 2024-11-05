0861ce20b31ec792bfe34d089aab94ea4d543c9a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-06 00:34:43 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from app_tweets import views as tweet_views
 from users import views as user_views
 
 urlpatterns = [
@@ -7,6 +8,6 @@ urlpatterns = [
     path('auth/login/', user_views.LoginView.as_view(), name='login'),
     path('auth/logout/', user_views.LogoutView.as_view(), name='logout'),
     path('verify-email/<uidb64>/<token>/', user_views.VerifyEmailView.as_view(), name='verify-email'),
-    path('verify-resend/', user_views.ResendEmailVerificationView.as_view(), name='verify-resend')
-
+    path('verify-resend/', user_views.ResendEmailVerificationView.as_view(), name='verify-resend'),
+    path('tweets/', tweet_views.TweetView.as_view(), name='tweets'),
 ]
